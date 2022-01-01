2aca243e78e5c8ad2c82916b15e2d7230eacdbe0
Ihor Bodnarchuk <ihor.bodnarchuk@iprocedures.com>
2021-12-31 21:43:09 -0600

    added search

--- components/dashboard/SmartContractsTab.vue
+++ components/dashboard/SmartContractsTab.vue
@@ -1,43 +1,48 @@
 <template>
-	<b-card-group v-if="items.length > 0" class="m-3" deck>
-		<b-card v-for="sc in items" :key="sc.id">
-			<template #header>
-				<h6 class="mb-0">{{ sc.name | startCase }}</h6>
-			</template>
-			<b-card-text>
-				<ul class="px-3">
-					<li>Symbol: {{ sc.symbol }}</li>
-					<li>Price: {{ sc.mintPrice }} {{ getCurrency(sc.chainId) }}</li>
-					<li>Blockchain: {{ sc.blockchain | blockchainName }}</li>
-					<li>Network: {{ getNetwork(sc.chainId) }}</li>
-					<li>Whitelist: {{ sc.hasWhitelist | yesNo }}</li>
-					<li>Delayed Reveal: {{ sc.hasDelayedReveal | yesNo }}</li>
-                    <li class="text-info">Status: {{sc.isDeployed ? 'Live': 'Draft'}}</li>
-					<li>Created: {{ sc.createdOn | toDate }}</li>
-				</ul>
-			</b-card-text>
-			<template #footer>
-				<b-button
-					v-if="sc.isDeployed"
-					variant="primary"
-					size="sm"
-					target="_blank"
-					:href="`${getExplorerUrl(sc.chainId)}/address/${sc.address}`"
-					>View <b-icon icon="box-arrow-up-right"/></b-button
-				>
-				<b-button
-					v-if="sc.isDeployed"
-					variant="info"
-					size="sm"
-					:to="`/smart-contracts/${sc.id}`"
-					>Interact</b-button
-				>
-				<b-button v-else variant="primary" size="sm" @click="onEdit(sc)"
-					>Edit/Deploy</b-button
-				>
-			</template>
-		</b-card>
-	</b-card-group>
+	<div v-if="items.length > 0" class="m-3">
+		<b-input-group size="lg" prepend="Seach:" class="mb-3">
+			<b-form-input @input="val => searchTerm = val" debounce="500"/>
+		</b-input-group>
+		<b-card-group deck>
+			<b-card v-for="sc in filteredItems" :key="sc.id">
+				<template #header>
+					<h6 class="mb-0">{{ sc.name | startCase }}</h6>
+				</template>
+				<b-card-text>
+					<ul class="px-3">
+						<li>Symbol: {{ sc.symbol }}</li>
+						<li>Price: {{ sc.mintPrice }} {{ getCurrency(sc.chainId) }}</li>
+						<li>Blockchain: {{ sc.blockchain | blockchainName }}</li>
+						<li>Network: {{ getNetwork(sc.chainId) }}</li>
+						<li>Whitelist: {{ sc.hasWhitelist | yesNo }}</li>
+						<li>Delayed Reveal: {{ sc.hasDelayedReveal | yesNo }}</li>
+						<li class="text-info">Status: {{sc.isDeployed ? 'Live': 'Draft'}}</li>
+						<li>Created: {{ sc.createdOn | toDate }}</li>
+					</ul>
+				</b-card-text>
+				<template #footer>
+					<b-button
+						v-if="sc.isDeployed"
+						variant="primary"
+						size="sm"
+						target="_blank"
+						:href="`${getExplorerUrl(sc.chainId)}/address/${sc.address}`"
+						>View <b-icon icon="box-arrow-up-right"/></b-button
+					>
+					<b-button
+						v-if="sc.isDeployed"
+						variant="info"
+						size="sm"
+						:to="`/smart-contracts/${sc.id}`"
+						>Interact</b-button
+					>
+					<b-button v-else variant="primary" size="sm" @click="onEdit(sc)"
+						>Edit/Deploy</b-button
+					>
+				</template>
+			</b-card>
+		</b-card-group>
+	</div>
 	<div v-else class="d-flex justify-content-center align-items-center h-100">
 		<div class="d-flex flex-column">
 			<h3>You don't have any smart contracts yet</h3>
@@ -54,6 +59,7 @@ import { getExplorerUrl, getCurrency, getNetwork, isTestnet } from '@/constants/
 
 export default {
 	data: () => ({
+		searchTerm: '',
 		items: [],
 		deployedAddress: null,
 		isBusy: false
@@ -67,6 +73,11 @@ export default {
 		})
 		this.items = data
 		console.log('smart-contracts-tab', this.items)
+	},
+	computed: {
+		filteredItems() {
+			return this.items.filter(x => x.name.includes(this.searchTerm) || x.symbol.includes(this.searchTerm))
+		}
 	},
 	methods: {
 		...mapMutations(['updateSmartContractBuilder']),
