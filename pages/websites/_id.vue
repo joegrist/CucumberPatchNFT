8acdfef1805ef548e0ddb075b76633d1ad8c3a46
Ihor Bodnarchuk <ihorbond@gmail.com>
2022-03-22 19:46:39 -0500

    added previews

--- pages/websites/_id.vue
+++ pages/websites/_id.vue
@@ -1,183 +1,218 @@
 <template>
-	<b-container class="pt-3">
-		<h1 class="text-center">{{ site.title }} </h1>
-		<div v-if="showPreview" v-html="iframeCode"></div>
-		<h5 class="text-center text-muted mb-3">Mint Page Preview</h5>
-		<div>
-			<ul>
-				<template v-if="site.type === WEBSITE_TYPE.Full">
-					<li>
-						<span class="text-success"
-							>Status: {{ WEBSITE_STATUS[site.status] }}</span
-						>
-					</li>
-					<li>
-						Site URL:
-						<ExternalLink :href="site.url" :text="site.url" />
-					</li>
-				</template>
-				<li>To embed on your site paste the following code into an HTML embed element
-						<br/>
-						<code>
-							{{ iframeCode }}
-						</code>
-						<Copy :value="iframeCode" />
-						<br/>
-						<ExternalLink href="https://www.youtube.com/watch?v=uo7mtt510hg&t=7s" text="See video" icon="youtube" />
-				</li>
-			</ul>
-		</div>
-		<b-form @submit.prevent="onUpdate" class="mb-3">
-			<b-form-group
-				label="Title"
-				label-class="required"
-				>
-				<b-form-input
-					id="title"
-					name="title"
-					v-model="site.title"
-					type="text"
-					placeholder="Bored Apes Yacht Club"
-					required></b-form-input>
-			</b-form-group>
-			<b-form-group label="Description">
-				<b-form-textarea
-					id="description"
-					name="description"
-					v-model="site.description"></b-form-textarea>
-			</b-form-group>
-			<div class="d-flex flex-row mb-1">
-				<b-form-checkbox
-					id="isCounterHidden"
-					name="isCounterHidden"
-					switch
-					size="lg"
-					:checked="!site.isCounterHidden"
-					@input="val => site.isCounterHidden = !val"
-				></b-form-checkbox>
-				<div class="mt-1">
-					<label class="mb-0">Minted/Collection Size counter</label>
+	<b-container class="pt-2">
+		<h1 class="text-center my-3">{{ site.title }}</h1>
+		<b-row v-if="site.type !== WEBSITE_TYPE.Full">
+			<b-col cols="9" style="overflow: auto" class="text-center">
+				<div v-if="showPreview" v-html="iframeCode"></div>
+				<h5 class="text-center text-muted mb-3">Preview window</h5>
+			</b-col>
+			<b-col cols="3">
+				<b-form-group label="Template">
+					<b-form-select
+						v-model="iframe.view"
+						:options="viewOptions"></b-form-select>
+				</b-form-group>
+				<b-form-group :label="`Width (${iframe.width}%)`">
+					<b-form-input
+						v-model="iframe.width"
+						type="range"
+						max="100"></b-form-input>
+				</b-form-group>
+				<b-form-group :label="`Height (${iframe.height}px)`">
+					<b-form-input
+						v-model="iframe.height"
+						type="range"
+						max="2000"></b-form-input>
+				</b-form-group>
+				Embed code <Copy :value="iframeCode" />
+				<br />
+				<div class="border rounded p-2">
+					<code>
+						{{ iframeCode }}
+					</code>
 				</div>
-			</div>
-			<div class="d-flex">
-				<b-form-group
-					label="Icon"
-					description="Your site's icon. Max 1 MB in size"
-					class="pr-1 w-50">
-					<b-form-file
-						v-model="site.icon"
-						name="icon"
-						accept="image/*"
-						placeholder="Choose a file or drop it here..."
-						drop-placeholder="Drop file here..."></b-form-file>
-				</b-form-group>
-				<b-form-group
-					label="Background Image"
-					description="Your site's background. Max 1 MB in size"
-					class="pr-1 w-50">
-					<b-form-file
-						v-model="site.backgroundImage"
-						name="backgroundImage"
-						accept="image/*"
-						placeholder="Choose a file or drop it here..."
-						drop-placeholder="Drop file here..."></b-form-file>
-				</b-form-group>
-			</div>
-			<div class="d-flex">
-				<b-form-group
-					label="Drop Date"
-					description="Sets the countdown timer, enter your local time"
-					class="pr-1 w-50">
-					<b-form-input
-						id="dropDateInput"
-						name="dropDateInput"
-						v-model="site.dropDateInput"
-						type="date"
-					></b-form-input>
-				</b-form-group>
-				<b-form-group
-					label="Drop Time"
-					description="Sets the countdown timer"
-					class="w-50">
-					<b-form-input
-						id="dropTimeInput"
-						name="dropTimeInput"
-						v-model="site.dropTimeInput"
-						type="time"></b-form-input>
-				</b-form-group>
-			</div>
-			<div class="d-flex">
-				<b-form-group class="pr-1 w-50">
-					<template #label>
-						<ExternalLink :href="site.twitterURL" text="Twitter URL" />
-					</template>
-					<b-form-input
-						id="twitterURL"
-						name="twitterURL"
-						type="url"
-						v-model="site.twitterURL"></b-form-input>
-				</b-form-group>
-				<b-form-group class="w-50">
-					<template #label>
-						<ExternalLink :href="site.discordURL" text="Discord URL" />
-					</template>
-					<b-form-input
-						id="discordURL"
-						name="discordURL"
-						v-model="site.discordURL"></b-form-input>
-				</b-form-group>
-			</div>
-			<div class="d-flex">
-				<b-form-group class="pr-1 w-50">
-					<template #label>
-						<ExternalLink :href="site.instagramURL" text="Instagram URL" />
-					</template>
-					<b-form-input
-						id="instagramURL"
-						name="instagramURL"
-						v-model="site.instagramURL"></b-form-input>
-				</b-form-group>
-				<b-form-group class="w-50">
-					<template #label>
-						<ExternalLink :href="site.tikTokURL" text="TikTok URL" />
-					</template>
-					<b-form-input
-						id="tikTokURL"
-						name="tikTokURL"
-						v-model="site.tikTokURL"></b-form-input>
-				</b-form-group>
-			</div>
-			<div class="d-flex">
-				<b-form-group class="pr-1 w-50">
-					<template #label>
-						<ExternalLink :href="site.marketplaceURL" text="Marketplace URL" />
-					</template>
-					<b-form-input
-						id="marketplaceURL"
-						name="marketplaceURL"
-						v-model="site.marketplaceURL"></b-form-input>
-				</b-form-group>
-				<b-form-group class="w-50" description="Etherscan, snowtrace, etc.">
-					<template #label>
-						<ExternalLink :href="site.blockExplorerUrl" text="Block Explorer URL" />
-					</template>
-					<b-form-input
-						id="blockExplorerUrl"
-						name="blockExplorerUrl"
-						v-model="site.blockExplorerUrl">
-					</b-form-input>
-				</b-form-group>
-			</div>
-			<div class="d-flex justify-content-end">
-				<b-button
-					variant="danger"
-					v-b-modal="site.id"
-					:disabled="isBusy || site.status !== WEBSITE_STATUS.Ready"
-					>Delete</b-button
-				>
-				<b-button class="ml-2" type="submit" variant="success">Update</b-button>
-			</div>
-		</b-form>
+				<ExternalLink
+					href="https://www.youtube.com/watch?v=uo7mtt510hg&t=7s"
+					text="How to video"
+					icon="youtube" />
+			</b-col>
+		</b-row>
+		<b-row>
+			<b-col>
+				<div>
+					<ul>
+						<template v-if="site.type === WEBSITE_TYPE.Full">
+							<li>
+								Site URL:
+								<ExternalLink :href="site.url" :text="site.url" />
+							</li>
+						</template>
+					</ul>
+				</div>
+			</b-col>
+		</b-row>
+		<b-row>
+			<b-col>
+				<b-form @submit.prevent="onUpdate" class="mb-3">
+					<b-form-group label="Title" label-class="required">
+						<b-form-input
+							id="title"
+							name="title"
+							v-model="site.title"
+							type="text"
+							placeholder="Bored Apes Yacht Club"
+							required></b-form-input>
+					</b-form-group>
+					<b-form-group label="Description">
+						<b-form-textarea
+							id="description"
+							name="description"
+							v-model="site.description"></b-form-textarea>
+					</b-form-group>
+					<div class="d-flex flex-row mb-1">
+						<b-form-checkbox
+							id="isCounterHidden"
+							name="isCounterHidden"
+							switch
+							size="lg"
+							:checked="!site.isCounterHidden"
+							@input="(val) => (site.isCounterHidden = !val)"></b-form-checkbox>
+						<div class="mt-1">
+							<label class="mb-0">Minted/Collection Size counter</label>
+						</div>
+					</div>
+					<div v-show="iframe.view === ''">
+						<div class="d-flex">
+							<b-form-group
+								label="Drop Date"
+								description="Sets the countdown timer, enter your local time"
+								class="pr-1 w-50">
+								<b-form-input
+									id="dropDateInput"
+									name="dropDateInput"
+									v-model="site.dropDateInput"
+									type="date"></b-form-input>
+							</b-form-group>
+							<b-form-group
+								label="Drop Time"
+								description="Sets the countdown timer"
+								class="w-50">
+								<b-form-input
+									id="dropTimeInput"
+									name="dropTimeInput"
+									v-model="site.dropTimeInput"
+									type="time"></b-form-input>
+							</b-form-group>
+						</div>
+						<div class="d-flex">
+							<b-form-group
+								label="Icon"
+								description="Your site's icon. Max 1 MB in size"
+								class="pr-1 w-50">
+								<b-form-file
+									v-model="site.icon"
+									name="icon"
+									accept="image/*"
+									placeholder="Choose a file or drop it here..."
+									drop-placeholder="Drop file here..."></b-form-file>
+							</b-form-group>
+							<b-form-group
+								label="Background Image"
+								description="Your site's background. Max 1 MB in size"
+								class="pr-1 w-50">
+								<b-form-file
+									v-model="site.backgroundImage"
+									name="backgroundImage"
+									accept="image/*"
+									placeholder="Choose a file or drop it here..."
+									drop-placeholder="Drop file here..."></b-form-file>
+							</b-form-group>
+						</div>
+						<div class="d-flex">
+							<b-form-group class="pr-1 w-50">
+								<template #label>
+									<ExternalLink :href="site.twitterURL" text="Twitter URL" />
+								</template>
+								<b-form-input
+									id="twitterURL"
+									name="twitterURL"
+									type="url"
+									v-model="site.twitterURL"></b-form-input>
+							</b-form-group>
+							<b-form-group class="w-50">
+								<template #label>
+									<ExternalLink :href="site.discordURL" text="Discord URL" />
+								</template>
+								<b-form-input
+									id="discordURL"
+									name="discordURL"
+									v-model="site.discordURL"></b-form-input>
+							</b-form-group>
+						</div>
+						<div class="d-flex">
+							<b-form-group class="pr-1 w-50">
+								<template #label>
+									<ExternalLink
+										:href="site.instagramURL"
+										text="Instagram URL" />
+								</template>
+								<b-form-input
+									id="instagramURL"
+									name="instagramURL"
+									v-model="site.instagramURL"></b-form-input>
+							</b-form-group>
+							<b-form-group class="w-50">
+								<template #label>
+									<ExternalLink :href="site.tikTokURL" text="TikTok URL" />
+								</template>
+								<b-form-input
+									id="tikTokURL"
+									name="tikTokURL"
+									v-model="site.tikTokURL"></b-form-input>
+							</b-form-group>
+						</div>
+						<div class="d-flex">
+							<b-form-group class="pr-1 w-50">
+								<template #label>
+									<ExternalLink
+										:href="site.marketplaceURL"
+										text="Marketplace URL" />
+								</template>
+								<b-form-input
+									id="marketplaceURL"
+									name="marketplaceURL"
+									v-model="site.marketplaceURL"></b-form-input>
+							</b-form-group>
+							<b-form-group
+								class="w-50"
+								description="Etherscan, snowtrace, etc.">
+								<template #label>
+									<ExternalLink
+										:href="site.blockExplorerUrl"
+										text="Block Explorer URL" />
+								</template>
+								<b-form-input
+									id="blockExplorerUrl"
+									name="blockExplorerUrl"
+									v-model="site.blockExplorerUrl">
+								</b-form-input>
+							</b-form-group>
+						</div>
+					</div>
+					<div class="d-flex justify-content-end">
+						<b-button
+							variant="danger"
+							v-b-modal="site.id"
+							:disabled="isBusy || site.status !== WEBSITE_STATUS.Ready"
+							>Delete</b-button
+						>
+						<b-button class="ml-2" type="submit" variant="success"
+							>Update</b-button
+						>
+					</div>
+				</b-form>
+			</b-col>
+		</b-row>
 		<b-modal
 			:id="site.id"
 			title="Confirm"
@@ -186,9 +221,8 @@
 			ok-variant="success"
 			ok-title="Yes"
 			cancel-title="No"
-			@ok="onDelete"
-		>
-			<h5>Are you sure want to delete this website ?</h5>
+			@ok="onDelete">
+			<h5>Are you sure want to delete this page ?</h5>
 		</b-modal>
 	</b-container>
 </template>
@@ -202,7 +236,6 @@ import timezone from 'dayjs/plugin/timezone'
 dayjs.extend(utc)
 dayjs.extend(timezone)
 
-
 export default {
 	middleware: 'authenticated',
 	data() {
@@ -210,7 +243,16 @@ export default {
 			WEBSITE_STATUS,
 			WEBSITE_TYPE,
 			site: {},
-			showPreview: true
+			showPreview: true,
+			iframe: {
+				view: 'tabs',
+				width: 100,
+				height: 170,
+			},
+			viewOptions: [
+				{ value: 'tabs', text: 'Tabs' },
+				{ value: '', text: 'Full' },
+			],
 		}
 	},
 	fetchOnServer: false,
@@ -219,7 +261,11 @@ export default {
 		const { data } = await this.$axios.get(`/websites/${this.$route.params.id}`)
 		this.site = data
 		if (this.site.dropDate) {
-			const [date, time] = dayjs.utc(this.site.dropDate).tz(this.site.timeZone || dayjs.tz.guess()).format('YYYY-MM-DDTHH:mm:ss').split('T')
+			const [date, time] = dayjs
+				.utc(this.site.dropDate)
+				.tz(this.site.timeZone || dayjs.tz.guess())
+				.format('YYYY-MM-DDTHH:mm:ss')
+				.split('T')
 			this.site.dropDateInput = date
 			this.site.dropTimeInput = time
 		}
@@ -230,13 +276,13 @@ export default {
 		iframeCode() {
 			return `
 				<iframe 
-					width="100%" 
-					height="800px"
+					width="${this.iframe.width}%" 
+					height="${this.iframe.height}px"
 					allowfullscreen="true"
 					loading="lazy"
 					title="${this.site.title}"
-					src="${this.$config.MINT_SITE_URL}?siteId=${this.site.id}"></iframe>`
-		}
+					src="${this.$config.MINT_SITE_URL}/${this.iframe.view}?siteId=${this.site.id}"></iframe>`
+		},
 	},
 	methods: {
 		...mapMutations(['setBusy']),
@@ -255,13 +301,13 @@ export default {
 			}
 		},
 		async onUpdate() {
-			if(this.site.backgroundImage?.size / (1024*1024) > 1) {
-				alert("Background image is too big. Max supported size is 1 mb.")
+			if (this.site.backgroundImage?.size / (1024 * 1024) > 1) {
+				alert('Background image is too big. Max supported size is 1 mb.')
 				return
 			}
 
-			if(this.site.icon?.size / (1024*1024) > 1) {
-				alert("Icon is too big. Max supported size is 1 mb.")
+			if (this.site.icon?.size / (1024 * 1024) > 1) {
+				alert('Icon is too big. Max supported size is 1 mb.')
 				return
 			}
 
@@ -274,7 +320,9 @@ export default {
 				update.dropTimeZone = dayjs.tz.guess()
 				update.dropDate = dropDateInput
 				if (dropDateInput && dropTimeInput) {
-					update.dropDate = new Date(`${dropDateInput}T${dropTimeInput}`).toUTCString()
+					update.dropDate = new Date(
+						`${dropDateInput}T${dropTimeInput}`
+					).toUTCString()
 				}
 
 				const payload = Object.keys(update).reduce((formData, key) => {
@@ -286,15 +334,12 @@ export default {
 
 				// reload iframe
 				this.showPreview = false
-				setTimeout(() => this.showPreview = true, 1000)
+				setTimeout(() => (this.showPreview = true), 1000)
 
-				this.$bvToast.toast(
-					'Website updated successfully',
-					{
-						title: 'Website',
-						variant: 'success',
-					}
-				)
+				this.$bvToast.toast('Website updated successfully', {
+					title: 'Website',
+					variant: 'success',
+				})
 			} catch (err) {
 				this.$bvToast.toast(err.message || 'Failed to update the website', {
 					title: 'Website',
