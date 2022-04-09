7fb4c16a86956057bce56f557900c5ba4ac96f21
Ihor Bodnarchuk <ihorbond@gmail.com>
2022-04-09 14:16:44 -0500

    refactor whitelist

--- pages/smart-contracts/_id.vue
+++ pages/smart-contracts/_id.vue
@@ -244,74 +244,7 @@
 				</b-row>
 			</b-tab>
 			<b-tab v-if="rawContract.hasWhitelist" title="Whitelist" lazy>
-				<b-row>
-					<b-col class="mb-3">
-						<b-row class="mb-2">
-							<b-col>
-								<h4 class="m-0">Whitelist</h4>
-								<b-link href="/whitelist-csv-example.csv" download
-									>Download example</b-link
-								>
-							</b-col>
-						</b-row>
-						<b-row no-gutters>
-							<b-col>
-								<b-form-file
-									accept=".csv"
-									@input="onImportCsv"
-									placeholder="Choose or drop .csv file"
-									drop-placeholder="Drop file here...">
-								</b-form-file>
-								<div v-if="invalidAddresses.length > 0">
-									<h5 class="text-danger">Invalid addresses</h5>
-									<ul class="p-0">
-										<li
-											class="d-flex"
-											v-for="(addr, idx) in invalidAddresses"
-											:key="idx">
-											{{ addr }}
-											<b-icon
-												class="my-auto"
-												icon="trash"
-												variant="danger"
-												@click="onRemoveInvalidAddress(addr)"></b-icon>
-										</li>
-									</ul>
-								</div>
-							</b-col>
-						</b-row>
-						<b-row class="my-2">
-							<b-col cols="6">
-								<b-button
-									block
-									variant="warning"
-									:disabled="isProcessingWhitelistCommit"
-									@click="onWhitelistCommit"
-									>Commit List</b-button
-								>
-							</b-col>
-							<b-col cols="6">
-								<b-button
-									block
-									variant="danger"
-									:disabled="isProcessingWhitelistCommit"
-									@click="onClearWhitelist"
-									>Clear</b-button
-								>
-							</b-col>
-						</b-row>
-						<b-row>
-							<b-col>
-								<b-form-tags
-									v-model="rawContract.whitelist"
-									invalid-tag-text="Address is invalid"
-									:tag-validator="whitelistValidator"
-									placeholder="Enter Wallet Address">
-								</b-form-tags>
-							</b-col>
-						</b-row>
-					</b-col>
-				</b-row>
+				<Whitelist :smartContract="rawContract" />
 			</b-tab>
 			<b-tab title="Mint Page" lazy>
 				<MintPage :smartContractId="rawContract.id" />
@@ -417,8 +350,9 @@ import {
 import { ethers } from 'ethers'
 import { isNumber, startCase } from 'lodash-es'
 import { loadScript } from '@paypal/paypal-js'
-import { getMerkleRoot, downloadTextFile } from '@/utils'
+import { downloadTextFile } from '@/utils'
 import MintPage from '@/components/smart-contract-dashboard/MintPage'
+import Whitelist from '@/components/smart-contract-dashboard/Whitelist'
 
 const basicFunctions = [
 	'airdrop',
@@ -438,12 +372,12 @@ const basicFunctions = [
 export default {
 	middleware: 'authenticated',
 	components: {
-		MintPage
+		MintPage,
+		Whitelist
 	},
 	data: () => ({
 		SMARTCONTRACT_STATUS,
 		SALE_STATUS,
-		invalidAddresses: [],
 		showAdvancedFunctions: false,
 		rawContract: {},
 		contract: {},
@@ -454,7 +388,6 @@ export default {
 		saleStatus: 'N/A',
 		busyState: {},
 		isReady: false,
-		isProcessingWhitelistCommit: false,
 		paypal: null,
 		ethPayTxHash: null,
 		currentOwner: null
@@ -469,9 +402,6 @@ export default {
 				`/users/${this.userId}/smartcontracts/${this.$route.params.id}`
 			)
 			this.rawContract = data
-			this.rawContract.whitelist = this.rawContract.whitelist.filter(
-				(a) => a !== ethers.utils.AddressZero
-			)
 
 			const { address, abi } = this.rawContract
 
@@ -588,15 +518,6 @@ export default {
 				})
 			}
 		},
-		whitelistValidator(tag) {
-			return ethers.utils.isAddress(tag)
-		},
-		onRemoveInvalidAddress(a) {
-			this.invalidAddresses = this.invalidAddresses.filter((x) => x !== a)
-			this.rawContract.whitelist = this.rawContract.whitelist.filter(
-				(x) => x !== a
-			)
-		},
 		async payWithEth() {
 			try {
 				const data = await fetch(
@@ -632,104 +553,6 @@ export default {
 						variant: 'danger',
 					}
 				)
-			}
-		},
-		async onWhitelistCommit() {
-			this.setBusy({isBusy: true})
-			try {
-				this.rawContract.whitelist.forEach((a) => {
-					if (!ethers.utils.isAddress(a)) {
-						this.invalidAddresses.push(a)
-					}
-				})
-
-				if (this.invalidAddresses.length > 0) {
-					this.$bvToast.toast(
-						'Whitelist contains invalid addresses. Please remove and resubmit',
-						{
-							title: 'Whitelist',
-							variant: 'danger',
-						}
-					)
-					return
-				}
-
-				await this.$axios.patch(
-					`/smartcontracts/${this.rawContract.id}/whitelist`,
-					{
-						whitelist: this.rawContract.whitelist,
-					}
-				)
-
-				if (this.rawContract.whitelist.length === 0) {
-					this.rawContract.whitelist = [ethers.constants.AddressZero]
-				}
-				const merkleRoot = getMerkleRoot(this.rawContract.whitelist)
-				const txResponse = await this.contract.setMerkleRoot(merkleRoot)
-				this.isProcessingWhitelistCommit = true
-
-				const msg = [this.createToastMessage(txResponse.hash)]
-				this.$bvToast.toast(msg, {
-					title: `Processing 'Commit List'`,
-					variant: 'success',
-				})
-				txResponse.wait().then(async (res) => {
-					this.isProcessingWhitelistCommit = false
-					this.$bvToast.toast(msg, {
-						title: 'Commit List',
-						variant: 'success',
-					})
-				})
-			} catch (err) {
-				console.log({ err })
-				this.$bvToast.toast('Whitelist commit failed', {
-					title: 'Whitelist',
-					variant: 'danger',
-				})
-			} finally {
-				this.isProcessingWhitelistCommit = false
-				this.setBusy({isBusy: false})
-			}
-		},
-		async onClearWhitelist() {
-			this.invalidAddresses = []
-			this.rawContract.whitelist = []
-		},
-		async onImportCsv(file) {
-			if (file === null) return
-
-			try {
-				const form = new FormData()
-				form.append('file', file)
-
-				const { data } = await this.$axios.post(
-					`/smartcontracts/${this.rawContract.id}/whitelist`,
-					form
-				)
-
-				this.rawContract.whitelist = data
-
-				this.$bvToast.toast(
-					'You MUST commit the list to save it into the smart contract for it to take effect!',
-					{
-						title: 'Whitelist',
-						variant: 'warning',
-					}
-				)
-				this.$bvToast.toast('File successfully uploaded', {
-					title: 'Whitelist',
-					variant: 'success',
-				})
-				this.invalidAddresses = []
-			} catch (err) {
-				console.log({ err })
-				if (err.response.data.errors?.invalidAddresses) {
-					this.invalidAddresses = err.response.data.errors?.invalidAddresses
-				}
-				this.$bvToast.toast('File upload failed', {
-					title: 'Whitelist',
-					variant: 'danger',
-				})
 			}
 		},
 		async onRefreshBalance(showNotification = false) {
