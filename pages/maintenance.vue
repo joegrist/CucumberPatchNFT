29689a8ff0fe12a69c12744d4bc016e2dc29a31e
Ihor Bodnarchuk <ihorbond@gmail.com>
2022-05-02 16:27:53 -0500

    wip

---
+++ pages/maintenance.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="container" class="d-flex justify-content-center align-items-center">
+        <h1>The application is receiving updates. We'll be back shortly.</h1>
+    </div>
+</template>
+
+<style>
+
+#container {
+    height: calc(100vh - 150px);
+}
+
+</style>
