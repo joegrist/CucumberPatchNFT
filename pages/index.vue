65bb5687e214a7f8a18f9bdd9c7a751e071b74e6
Ihor Bodnarchuk <ihorbond@gmail.com>
2022-05-02 15:52:08 -0500

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -1,81 +1,13 @@
 <template>
-	<div class="p-3">
-		<div class="d-flex justify-content-between">
-			<b-input-group class="mb-3">
-				<b-input-group-prepend>
-					<b-input-group-text>
-						<b-icon icon="search" />
-					</b-input-group-text>
-				</b-input-group-prepend>
-				<b-form-input
-					@input="(val) => (searchTerm = val)"
-					debounce="500"
-					placeholder="Start typing the smart contract name.." />
-				<b-input-group-append>
-					<b-button variant="primary" class="border-0" to="/wizard"
-						>Add New</b-button
-					>
-				</b-input-group-append>
-			</b-input-group>
-		</div>
-		<b-card-group v-if="dashboardItems.length > 0" columns>
-			<DashboardCard
-				v-for="sc in filteredItems"
-				:key="sc.id"
-				:sc="sc"
-			/>
-		</b-card-group>
-		<div v-else class="text-center pt-2">
-			<h1>You Don't Have Any Projects Yet</h1>
-			<b-button
-				variant="primary"
-				@click="$router.push('/wizard')"
-				>Create One</b-button
-			>
-		</div>
-	</div>
+    <div id="container" class="d-flex justify-content-center align-items-center">
+        <h1>The application is receiving updates. We'll be back shortly.</h1>
+    </div>
 </template>
 
-<script>
-import { mapMutations, mapActions, mapGetters, mapState } from 'vuex'
-import { MARKETPLACE } from '@/constants'
+<style>
 
-export default {
-	middleware: 'authenticated',
-	data() {
-		return {
-			MARKETPLACE,
-			searchTerm: '',
-		}
-	},
-	fetchOnServer: false,
-	fetchKey: 'dashboard',
-	async fetch() {
-		await this.loadDashboardCards()
-	},
-	created(){
-		const refCode = this.$route.query['ref']
-		if(refCode) {
-			sessionStorage.setItem('ref', refCode)
-		}
-	},
-	computed: {
-		...mapState(['dashboardItems', 'isBusy']),
-		...mapGetters(['userId']),
-		filteredItems() {
-			const term = this.searchTerm.toLowerCase()
-			return this.dashboardItems.filter(
-				(x) =>
-					x.name.toLowerCase().includes(term) ||
-					x.symbol.toLowerCase().includes(term)
-			)
-		},
-	},
-	methods: {
-		...mapMutations(['setDashboardItems', 'setBusy']),
-		...mapActions(['loadDashboardCards'])
-	},
+#container {
+    height: calc(100vh - 150px);
 }
-</script>
 
-<style lang="scss" scoped src="@/assets/styles/dashboard.scss"></style>
+</style>
